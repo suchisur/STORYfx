5aaac9648cfbd2e62f0ed7ae9d873de4fd419701
suchisur <suchisur29@gmail.com>
2020-07-08 18:54:51 +0530

    some useful material

---
+++ additonal UI material/config/secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  database: process.env.DATABASE || 'mongodb+srv://root:STORYfx123@storyfx.o3bvv.mongodb.net/STORYfx?retryWrites=true&w=majority',
+  port: process.env.PORT || 3000,
+  secret: process.env.SECRET || 'STORYfx123',
+
+}
