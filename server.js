9022212363a5b4bdafd4924c34db865b78fd1df8
Kshitij Agrawal <ksagrawal143@gmail.com>
2020-07-08 18:33:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
-require('dotenv').config();
 
-const uri = “mongodb+srv:root:storyfx@cluster0.isjic.mongodb.net/User?retryWrites=true&w=majority“;
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://root:storyfx@cluster0.isjic.mongodb.net/<dbname>?retryWrites=true&w=majority";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => {
-  console.log(‘MongoDB Connected…’)
-})
-.catch(err => console.log(err))
+  .then(() => {
+    console.log('MongoDB Connected…')
+  })
+  .catch(err => console.log(err))
 
-app.listen(process.env.PORT, () =>{
-    console.log(`server is running on port: ${process.env.PORT}`);
+
+app.listen(5000, () => {
+  console.log('server is running on port: 5000');
 });
