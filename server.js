434799fb7ddb8035f701656bbff3e0022309b029
Kshitij Agrawal <ksagrawal143@gmail.com>
2020-07-08 18:17:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ app.use(express.json());
 
 require('dotenv').config();
 
-mongoose.connect( process.env.ATLAS_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.once('open',() => {
-    console.log(`MongoDB database connection established`);
-});
-
+const uri = “mongodb+srv:root:storyfx@cluster0.isjic.mongodb.net/User?retryWrites=true&w=majority“;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log(‘MongoDB Connected…’)
+})
+.catch(err => console.log(err))
 
 app.listen(process.env.PORT, () =>{
     console.log(`server is running on port: ${process.env.PORT}`);
